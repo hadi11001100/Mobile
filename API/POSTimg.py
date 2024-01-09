58e7725194766b1e87c69a2ce3f84a549c2ce855
Ihr Name <hadi.el.sabbagh@gmail.com>
2024-01-09 09:56:27 +0100

    update

--- API/POSTimg.py
+++ API/POSTimg.py
@@ -12,15 +12,11 @@ UPLOAD_FOLDER = 'images'
 # Maximal zulässige Dateigröße
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
  
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
- 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
- 
 @app.route('/')
 def main():
-    return 'Homepage'
+    return 'Hauptseite'
  
+# files[] ist der Schlüssel 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # check if the post request has the file part
@@ -29,13 +25,14 @@ def upload_file():
         resp.status_code = 400
         return resp
  
+ # Das request-Objekt enthält Daten, die in einer HTTP-Anfrage gesendet werden
     files = request.files.getlist('files[]')
      
     errors = {}
     success = False
      
     for file in files:      
-        if file and allowed_file(file.filename):
+        if file: 
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             success = True
