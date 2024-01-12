05336fd664dc51d39652d6c520a944373d03e1b2
Ihr Name <hadi.el.sabbagh@gmail.com>
2024-01-12 10:42:40 +0100

    SmartAPP API

---
+++ API/SmartApp_API.py
@@ -0,0 +1,63 @@
+from flask import Flask, json, request, jsonify, send_from_directory
+import os
+import urllib.request
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+app = Flask(__name__)
+ 
+UPLOAD_FOLDER = 'images\input_Images'
+DOWNLOAD_FOLDER = 'images\output_Images'
+
+# Maximal zulässige Dateigröße
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ 
+@app.route('/')
+def main():
+    return 'Hauptseite'
+ 
+# files[] ist der Schlüssel 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'files[]' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+ 
+ # Das request-Objekt enthält Daten, die in einer HTTP-Anfrage gesendet werden
+    files = request.files.getlist('files[]')
+     
+    errors = {}
+    success = False
+     
+    for file in files:      
+        if file: 
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            success = True
+        else:
+            errors[file.filename] = 'File type is not allowed'
+ 
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    if success:
+        resp = jsonify({'message' : 'Files successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    
+# Man kann sich das Bild aus dem Ordner selber aussuchen 
+@app.route('/image/<filename>')
+def display_image(filename):
+    return send_from_directory('images\processed_Images', filename)
+
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
